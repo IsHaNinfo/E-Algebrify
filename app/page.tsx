db5be147bd1d3f078fad60c3187b2029face688d
ishaninfo <ishaninfo56@gmail.com>
2025-05-20 16:55:09 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import { buttonVariants } from "@/components/ui/button";
+import { page_routes } from "@/lib/routes-config";
+import { MoveUpRightIcon, TerminalSquareIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex sm:min-h-[87.5vh] min-h-[82vh] flex-col sm:items-center justify-center text-center sm:py-8 py-14">
+
+      <h1 className="text-[1.80rem] leading-8 sm:px-8 md:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
+        Master O/L Algebra with Confidence and Ease.
+      </h1>
+      <p className="mb-8 md:text-lg text-base  max-w-[1200px] text-muted-foreground text-left sm:text-center">
+        This comprehensive learning platform is tailored for Grade 11 students preparing for O/L exams.
+        Learn algebra concepts through structured lessons, interactive problem-solving, and real exam-style questions to boost your confidence and performance.
+      </p>
+      <div className="sm:flex sm:flex-row grid grid-cols-2 items-center sm;gap-5 gap-3 mb-8">
+        <Link
+          href={`/docs${page_routes[0].href}`}
+          className={buttonVariants({ className: "px-6", size: "lg" })}
+        >
+          Get Stared
+        </Link>
+        <Link
+          href="/blog"
+          className={buttonVariants({
+            variant: "secondary",
+            className: "px-6",
+            size: "lg",
+          })}
+        >
+          Read Blog
+        </Link>
+      </div>
+
+    </div>
+  );
+}
